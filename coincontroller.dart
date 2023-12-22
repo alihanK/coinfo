6d6537e1e5ac1d77d488d7b6790f818875ca8cda
alihanK <82571528+alihanK@users.noreply.github.com>
2023-12-22 18:01:07 +0300

    Add files via upload
    This App for display some crypto coins' price. Also this app always up to date. Developed all via Flutter.

---
+++ coincontroller.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: depend_on_referenced_packages, non_constant_identifier_names
+
+import 'package:coinfo/models/coinmodel.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+class CoinControls extends GetxController {
+  RxBool isLoading = true.obs;
+  RxList<Coin> CoinsList = <Coin>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetcCoins();
+  }
+
+  fetcCoins() async {
+    try {
+      isLoading(true);
+      var response =
+          await http.get(Uri.parse('get needed data codes from api'));
+
+      List<Coin> coins = coinFromJson(response.body);
+      CoinsList.value = coins;
+    } finally {
+      isLoading(false);
+    }
+  }
+}
